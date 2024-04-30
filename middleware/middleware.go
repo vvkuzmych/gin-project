0874e70d57b809d2c3b51ecd6772ae9df4ee7108
Volodymyr Kuzmych <44097750+vvkuzmych@users.noreply.github.com>
2024-04-30 11:47:05 +0300

    Update middleware.go

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,17 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//// Middleware function to log request information
-//func RouteMiddleware(next http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		// Log request information
-//		fmt.Printf("Request: %s %s\n", r.Method, r.URL.Path)
-//
-//		// Call the next handler in the chain
-//		next.ServeHTTP(w, r)
-//	})
-//}
-
 func RouteMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		
